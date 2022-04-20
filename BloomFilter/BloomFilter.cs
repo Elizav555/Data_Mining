4e8a6db90b823c808bbcb8420e0f2c55036e5253
Elizav555 <lizagarkina5@gmail.com>
2022-04-20 21:52:18 +0300

    done

---
+++ BloomFilter/BloomFilter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloomFilter
+{
+    public class BloomFilterClass
+    {
+        private int filterSize;
+        private int funcCount;
+        public int[] filter;
+        private HashFunc[] func;
+
+        public BloomFilterClass(int maxNumb, double errorProbability)
+        {
+            var optimalParams = GetOptimalFilterParams(maxNumb, errorProbability);
+            filterSize = optimalParams.Item1;
+            funcCount = optimalParams.Item2;
+            filter = new int[filterSize];
+            filter = filter.Select(it => 0).ToArray();
+            func = new HashFunc[funcCount];
+            for (int i = 0; i < funcCount; i++)
+            {
+                func[i] = new HashFunc();
+            }
+        }
+
+        public void Add(string str)
+        {
+            for (var i = 0; i < funcCount; i++)
+                filter[func[i].Hash(str) % filterSize] |= 1;
+        }
+
+        public bool FindString(string str)
+        {
+            for (var i = 0; i < funcCount; i++)
+                if (filter[func[i].Hash(str) % filterSize] == 0)
+                    return false;
+            return true;
+        }
+
+        private static (int, int) GetOptimalFilterParams(int elementsCount, double p)
+        {
+            var sizeDouble = -(elementsCount * Math.Log(p)) / (Math.Log(2) * Math.Log(2));
+            var countDouble = (sizeDouble / elementsCount) * Math.Log(2);
+            int size = (int)Math.Ceiling(sizeDouble);
+            int count = (int)Math.Ceiling(countDouble);
+            Console.WriteLine($"Optimal filter size : {size}");
+            Console.WriteLine($"Optimal func count : {count}");
+            return (size, count);
+        }
+    }
+}
---
+++ BloomFilter/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BloomFilter;
+
+var text = "";
+using (var reader = new StreamReader(Directory.GetParent(Directory.GetCurrentDirectory()).Parent + "/song.txt"))
+    text = reader.ReadToEnd();
+var separators = new char[] { ' ', '\n', ',', '.', '-', ':', ';', '?', '!', ')', '(', '\r' };
+var words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+words = words.Distinct().ToArray();
+var bloomFilter = new BloomFilterClass(words.Length, 0.1);
+foreach (var word in words)
+{
+    bloomFilter.Add(word);
+}
+foreach (var cell in bloomFilter.filter)
+    Console.Write(cell);
+Console.WriteLine();
+Console.WriteLine(bloomFilter.FindString("улыбки"));
+Console.WriteLine(bloomFilter.FindString("слону"));
+Console.WriteLine(bloomFilter.FindString("облака"));
+Console.WriteLine(bloomFilter.FindString("running"));
+Console.WriteLine(bloomFilter.FindString("word"));
+Console.WriteLine(bloomFilter.FindString("rrrrr"));
